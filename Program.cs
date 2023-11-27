438a113f5401d656e3af65c2fa40da1ccc48ff67
Alexey R <amareevich@gmail.com>
2023-11-27 19:20:23 +0300

    Добавил файл C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//1 Задача
+int[] arr = new int [10];
+Random rdm = new Random();
+
+for (int i = 0; i <= 9; i++ ){
+    arr[i] = rdm.Next(1,100);
+}
+
+int count = 0;
+
+foreach (int item in arr)
+{
+
+    if (item > 20 && item < 90) 
+    {
+        count++;
+    }
+
+};
+
+Console.WriteLine(count);
