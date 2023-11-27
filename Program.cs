2e7505ca3fe6a6c683e03404227ee4cc5aa6ca47
Alexey R <amareevich@gmail.com>
2023-11-27 20:37:42 +0300

    Добавил 4-ю задачу

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ foreach (int item in arr)
 Console.WriteLine($"В массиве {count} элементов лежат в диапозоне от 20 до 90");
 
 //*************************************Задача 2
+
 int[] arr = new int [10];
 Random rdm = new Random();
 
@@ -42,8 +43,9 @@ foreach (int item in arr)
 
 Console.WriteLine($"В массиве {count} четных элемента(ов)");*/
 
-//Задача 3
-double[] arr = new double [10];
+//****************************************Задача 3
+
+/*double[] arr = new double [10];
 Random rdm = new Random();
 
 for (int i = 0; i <= arr.Length - 1; i++ ){
@@ -64,4 +66,39 @@ for (int i = 0; i <= arr.Length - 1; i++) {
 
 double result = numMax - numMin;
 
-Console.WriteLine($" Разница между максимальным и минимальным элементом в массиве = {result}");
+Console.WriteLine($" Разница между максимальным и минимальным элементом в массиве = {result}");*/
+
+//*********************************Задача 4
+
+Random rdm = new Random();
+int num = rdm.Next(1,100000);
+string str = Convert.ToString(num);
+int[] arr = new int[str.Length];
+
+for (int i = 0; i <= str.Length - 1; i++) {
+    string elem = Convert.ToString(str[i]);
+    arr[i] = Convert.ToInt32(elem);
+}
+
+foreach (int elem in arr) {
+    Console.WriteLine(elem);
+}
+
+for (int i = 0; i <= arr.Length - 1; i++) {
+    for (int j = 0; j <= arr.Length - 1; j++) {
+        int number = arr[j];
+        if (arr[j] < arr[i]) {
+            arr[j] = arr[i];
+            arr[i] = number;
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+foreach (int elem in arr) {
+    Console.WriteLine(elem);
+}
+
